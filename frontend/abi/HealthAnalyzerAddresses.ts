f6a210b550165481575a07926a4ed42951a6002e
JillCatharine <dermojhanson@gmail.com>
2025-11-22 23:19:44 +0800

    Addresses mapping added [skip ci]

---
+++ frontend/abi/HealthAnalyzerAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const HealthAnalyzerAddresses = { 
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0x98eFcDd36fa0dc99F8e8bC8ECd361DeF48CAD361", chainId: 11155111, chainName: "sepolia" }
+};
